df50f0f0cb9ca3e8bc9ac4a447fec4bdd5ad2904
tomeksen <tomas.olmeda@hotmail.com>
2024-01-31 09:06:56 -0800

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using eCommerceSamotNet8.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceSamotNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Users>>> GetAllUsers()
+        {
+            var users = new List<Users> { new Users { Id = 1, Name = "John", Email = "" } };
+
+            return Ok(users);
+        }
+    }
+}
