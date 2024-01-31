19563cce5d8020ae50c1339740169ba6e15654b9
tomeksen <tomas.olmeda@hotmail.com>
2024-01-31 10:52:56 -0800

    Add datacontext and making initial migration

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using eCommerceSamotNet8.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceSamotNet8.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
